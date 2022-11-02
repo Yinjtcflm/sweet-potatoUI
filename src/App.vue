6beb6b507197b160a996d8f96ebe0f8adcbe99f8
Yinjtcflm <1529792769@163.com>
2022-11-02 17:44:39 +0800

    实现topnav和侧边栏交互

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <router-view />
 </template>
 
-<script>
+<script lang="ts">
+import { ref } from "@vue/reactivity";
+import { provide } from "vue";
 export default {
   name: "App",
+  setup() {
+    const menuVisible = ref(false);
+    provide("menuVisible", menuVisible);
+  },
 };
 </script>
