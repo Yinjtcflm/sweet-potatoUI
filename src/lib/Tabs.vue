0c3c2cddf9df83f8e5c3f8d8955d8530ba0ca041
Yinjtcflm <1529792769@163.com>
2022-11-10 00:01:04 +0800

    Tabs样式更改

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>Tabs组件</div>
-  <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-  <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="hs-tabs">
+    <div class="hs-tabs-nav">
+      <div class="hs-tabs-nav-item" v-for="(t, index) in titles" :key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="hs-tabs-content">
+      <component
+        class="hs-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { takeCoverage } from "v8";
@@ -21,3 +33,30 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.hs-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
