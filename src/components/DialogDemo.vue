70dda21cec6c0bbca82d8b79fa83b7d9b3e0f7d5
Yinjtcflm <1529792769@163.com>
2022-11-09 18:48:54 +0800

    openDialog一句话打开Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,38 +1,30 @@
 <template>
   <div>dialog示例</div>
   <h1>示例1</h1>
-  <div style="position: relative; z-index: 1">
-    <Button @click="toggle">toggle</Button>
-    <Dialog
-      v-model:visible="x"
-      :closeOnClickOverlay="false"
-      :ok="f1"
-      :cancel="f2"
-    >
-      <template v-slot:content>
-        <strong>红薯</strong>
-        <div>冬天必吃！</div>
-      </template>
-      <template v-slot:title>
-        <strong>这是一个提示</strong>
-      </template>
-    </Dialog>
-  </div>
-  <div
-    style="
-      position: relative;
-      z-index: 2;
-      width: 300px;
-      height: 300px;
-      background: red;
-    "
-  ></div>
+  <Button @click="toggle">toggle</Button>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  >
+    <template v-slot:content>
+      <strong>红薯</strong>
+      <div>冬天必吃！</div>
+    </template>
+    <template v-slot:title>
+      <strong>这是一个提示</strong>
+    </template>
+  </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
 import { ref } from "vue";
+import { openDialog } from "../lib/openDialog";
 
 export default {
   components: { Dialog, Button },
@@ -43,7 +35,19 @@ export default {
     };
     const f1 = () => {};
     const f2 = () => {};
-    return { x, toggle, f1, f2 };
+    const showDialog = () => {
+      openDialog({
+        title: "快来",
+        content: "买呀",
+        ok() {
+          console.log("ok");
+        },
+        cancel() {
+          console.log("cancel");
+        },
+      });
+    };
+    return { x, toggle, f1, f2, showDialog };
   },
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -56,7 +56,7 @@ export default {
       }
     };
     const cancel = () => {
-      context.emit("cancel");
+      props.cancel && props.cancel();
       close();
     };
     return { close, closeOnClickOverlay, ok, cancel };
