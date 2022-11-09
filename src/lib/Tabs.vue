0f2064fa48b6296483e9c84f1f61756c12b1c9b2
Yinjtcflm <1529792769@163.com>
2022-11-09 23:46:26 +0800

    渲染Tabs内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,23 @@
 <template>
   <div>Tabs组件</div>
+  <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+  <component v-for="(c, index) in defaults" :is="c" :key="index" />
 </template>
+<script lang="ts">
+import { takeCoverage } from "v8";
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs子标签必须是Tab");
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props!.title;
+    });
+    return { defaults, titles };
+  },
+};
+</script>
