b9d27693ce66f979d6fae5ef33da4159dbe4ec5a
Yinjtcflm <1529792769@163.com>
2022-11-11 23:17:20 +0800

    引入GitHub样式

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>intro</div>
+  <article class="markdown-body">
+    <h1>介绍</h1>
+    <p>Sweetpotato UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>
+      这款组件库主要是用于对Vue的进一步学习，包括父子组件如何传参，Vue3新特性学习
+    </p>
+    <p>下一节<a href="#doc/install">安装</a></p>
+  </article>
 </template>
